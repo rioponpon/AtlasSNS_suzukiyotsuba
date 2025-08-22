5886eb0c896d7c7250d83e3c3bd67f26359979bd
鈴木よつば <>
2025-08-22 22:06:48 +0900

    投稿フォームの設置（投稿処理エラー）

--- AtlasSNS_ver9/app/Http/Controllers/PostsController.php
+++ AtlasSNS_ver9/app/Http/Controllers/PostsController.php
@@ -10,4 +10,14 @@ class PostsController extends Controller
     public function index(){
         return view('posts.index');
     }
+    public function postCreate(Request $request){
+        $post=$request->input('newpost');
+        $user_id=Auth::user()->id;
+        dd($user_id);
+        post::create([
+            'user_id'=>$user_id,
+            'post'=>$post
+        ]);
+        return redirect('/top');
+    }
 }
--- AtlasSNS_ver9/resources/views/posts/index.blade.php
+++ AtlasSNS_ver9/resources/views/posts/index.blade.php
@@ -2,5 +2,14 @@
 
 
   <h2>機能を実装していきましょう。</h2>
+<div class="container">
 
+{!! Form::open(['url' => '/top'])!!}
+{{Form::token() }}
+<div class="from-group">
+  {{ Form::input('text','newpost',null,['required','class'=> 'form-control','placeholder' => '投稿内容を入力してください'])}}
+</div>
+<button type="submit" class="btn btn-success pull-right"><img src="imges/post.png" alt="送信"></button>
+{!! Form::close()!!}
+</div>
 </x-login-layout>
