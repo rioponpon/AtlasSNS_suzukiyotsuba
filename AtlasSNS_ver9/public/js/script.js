2f0bfdaeb72c5f88ca077a953abee3144989ea73
鈴木よつば <>
2025-08-27 15:49:30 +0900

    再度送信

---
+++ AtlasSNS_ver9/public/js/script.js
@@ -0,0 +1,18 @@
+// $(function () { // if document is ready
+//   alert('hello world')
+// });
+jQuery(function ($) {
+
+  $('.js-accordion-title').on('click', function () {
+    /*クリックでコンテンツを開閉*/
+    $(this).find('.ul');
+    /*矢印の向きを変更*/
+    $(this).toggleClass('open', 200);
+  }).next().hide();
+
+
+  $('.update-btn').on('click', function () {
+    $('.modal').toggleClass('open', 200);
+  });
+
+});
